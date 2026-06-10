04cc4857f6590cc25e63bb97b69ff769d38650a3
Namithaa R <namithaaravi2006@gmail.com>
2026-06-10 21:03:15 +0530

    Update shiftregister.v

--- day 2/shift _register/design/shiftregister.v
+++ day 2/shift _register/design/shiftregister.v
@@ -1,16 +1,13 @@
-`timescale 1ns / 1ps
-
 module universal_shift_register (
     input clk,                  
     input rst,                  
-    input [1:0] mode,           
+    input [1:0] mode,          
     input serial_in,            
     input [3:0] parallel_in,    
     output reg serial_out,      
-    output reg [3:0] parallel_out 
+    output reg [3:0] parallel_out
 );
 
-    
     reg [3:0] shift_reg;
 
     always @(posedge clk or posedge rst) begin
@@ -21,26 +18,24 @@ module universal_shift_register (
         end
         else begin
             case (mode)
-                
                 2'b00: begin
-                    shift_reg  <= {shift_reg[2:0], serial_in}; 
-                    serial_out <= shift_reg[3];               
+                    shift_reg  <= {shift_reg[2:0], serial_in};
+                    serial_out <= shift_reg[3];              
                 end
 
-                
                 2'b01: begin
-                    shift_reg    <= {shift_reg[2:0], serial_in}; 
-                    parallel_out <= {shift_reg[2:0], serial_in}; 
+                    shift_reg    <= {shift_reg[2:0], serial_in};
+                    parallel_out <= {shift_reg[2:0], serial_in};
                 end
-
 
                 2'b10: begin
-                    serial_out <= shift_reg[3];               
+                    serial_out <= shift_reg[3];              
                     shift_reg  <= {shift_reg[2:0], 1'b0};    
                 end
+
                 2'b11: begin
-                    shift_reg    <= parallel_in; 
-                    parallel_out <= parallel_in; 
+                    shift_reg    <= parallel_in;
+                    parallel_out <= parallel_in;
                 end
                 
                 default: begin
